e63d63a3a8231984267df202c969f9f9132e05d7
Mark Bell <me@markb.co.uk>
2016-06-13 12:10:55 +0100

    Add case fold param to reference match harness

--- c/wm.c
+++ c/wm.c
@@ -3,13 +3,18 @@
 
 main(int argc, char **argv)
 {
-    if(argc < 2) 
+    if(argc < 4) 
     {
-        printf("%s\n", "Usage: wm <pattern> <text>");
+        printf("%s\n", "Usage: wm <pattern> <text> <casefold>");
         return 1;
     }
 
-    int match = wildmatch(argv[1], argv[2], WM_PATHNAME, NULL);
+    int flags = WM_PATHNAME;
+
+    if(strcmp(argv[3], "1") == 0)
+        flags = WM_PATHNAME | WM_CASEFOLD;
+
+    int match = wildmatch(argv[1], argv[2], flags, NULL);
     printf("%i\n", match);
 
     return match;
